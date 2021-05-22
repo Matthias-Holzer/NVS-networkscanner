b7f8bc8b9e155bb277a6dd3da60d74a06cba2f36
Holzer Matthias <wien.matthias.holzer@gmail.com>
2021-05-22 15:41:43 +0200

    bugs fixed

--- main.py
+++ main.py
@@ -17,42 +17,34 @@ def get_args():
 
 def scan(ip):
     arp_req_frame = scapy.ARP(pdst=ip)
-
     broadcast_ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-
     broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-
     answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=1, verbose=False)[0]
     result = []
     for i in range(0, len(answered_list)):
         client_dict = {"ip": answered_list[i][1].psrc, "mac": answered_list[i][1].hwsrc}
         result.append(client_dict)
-
     return result
 
 def scanPort(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect((host, port))
-        print("Port open: " + str(port))
+        print("\tPort open: " + str(port))
         s.close()
     except:
-        print("Port closed: " + str(port))
-
+        print("\tPort closed: " + str(port))
 
 def display_result(result, input):
     print("-----------------------------------\nIP Address\tMAC Address\n-----------------------------------")
     for i in result:
         print("{}\t{}".format(i["ip"], i["mac"]))
-        ports = str(input).split(',')
-        for port in ports:
-            scanPort(i["ip"],port)
-
+        if(input is not None):
+            ports = str(input).split(',')
+            for port in ports:
+                scanPort(i["ip"],port)
 
 options = get_args()
 scanned_output = scan(options.network)
 display_result(scanned_output, options.port)
-
-
-
 print("done")
